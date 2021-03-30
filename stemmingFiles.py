ef0f816f8bbf503a2d2249666bfd5d174965821f
Syed Hubab <syedmohdhubab@gmail.com>
2021-03-30 14:04:17 +0500

    Snowball Stemmer on Tokens

---
+++ stemmingFiles.py
@@ -0,0 +1,28 @@
+import Stemmer
+stemmer = Stemmer.Stemmer('english')
+
+words = []
+
+for NUMBER in range(1, 51):
+    name = str(NUMBER) + ".txt"
+    path = "./CS317-w07-IR Dataset for A1/ShortStoriesCleaned/"+name
+    file = open(path, encoding='windows-1252')
+    words = file.readlines()
+    file.close()
+    
+    for i in range(len(words)):
+        words[i] = words[i].replace('\n', '')
+    
+    stems = stemmer.stemWords(words)
+
+    path = "./CS317-w07-IR Dataset for A1/ShortStoriesStemmed/" + name
+    file = open(path, "w")
+
+    for i in range(len(stems)):
+        # file.write(stems[i])
+        file.write(stems[i]+'\n')
+    
+    file.close()
+
+    print(NUMBER, " before ->", len(words))
+    print(NUMBER, " after ->", len(stems)) 
